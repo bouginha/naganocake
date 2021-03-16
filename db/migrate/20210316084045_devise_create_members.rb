637d2ad96188a3ae6652404361b8386e6e92c169
hirotaka-hakuta <hakuta.hirotaka@gmail.com>
2021-03-16 10:31:20 +0000

    memberカラム修正

--- db/migrate/20210316084045_devise_create_members.rb
+++ db/migrate/20210316084045_devise_create_members.rb
@@ -6,7 +6,14 @@ class DeviseCreateMembers < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :last_name
+      t.string :first_name
+      t.string :last_name_kana
+      t.string :first_name_kana
+      t.string :postal_code
+      t.string :address
+      t.string :telephone_number
+      t.boolean :is_deleted
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -35,6 +42,7 @@ class DeviseCreateMembers < ActiveRecord::Migration[5.2]
 
       t.timestamps null: false
     end
+
 
     add_index :members, :email,                unique: true
     add_index :members, :reset_password_token, unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(version: 2021_03_16_084047) do
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "postal_code"
+    t.string "address"
+    t.string "telephone_number"
+    t.boolean "is_deleted"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
