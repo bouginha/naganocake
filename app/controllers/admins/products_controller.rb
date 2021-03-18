3e1100b10351b43a40326bc467b9f20b77519649
hirotaka-hakuta <hakuta.hirotaka@gmail.com>
2021-03-18 07:25:07 +0000

    プロダクト途中

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -1,22 +1,33 @@
 class Admins::ProductsController < ApplicationController
-  def new
-  end
-
-  def show
-  end
-
-  def index
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def create
-  end
-
-  def destroy
-  end
+        def new
+            @product=Product.new
+        end
+        def create
+            @product=Product.new(product_params)
+            if @product.save
+              redirect_to products_path
+            else
+              render :new
+            end
+        end
+        def show
+        end
+        
+        def index
+            @products=Product.all
+            
+        end
+        
+        def edit
+        end
+        
+        def destroy
+        end
+        
+        def update
+        end
+     private   
+        def product_params
+          params.require(:product).permit(:name,:about,:image,:normal_price,:products_genre_id,:is_active)
+        end
 end
