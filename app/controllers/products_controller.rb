878471e9734756301d0d801ef79d072392a13b5e
Leica913 <toruneko1996913@gmail.com>
2021-03-21 09:22:41 +0000

    商品詳細画面(会員)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,16 @@
 class ProductsController < ApplicationController
   def show
-    @products=Product.find(product_params)
-
-    
+    @cart_product = CartProduct.new
+    @products_genre = ProductsGenre.all
+    @product = Product.find(params[:id])
   end
 
   def index
     @products=Product.all
     @products_genre = ProductsGenre.all
   end
-  
-  private   
+
+  private
     def product_params
       params.require(:product).permit(:name,:about,:image,:normal_price,:products_genre_id,:is_active)
     end
