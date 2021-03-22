84d4cafc996eae2da2ed496c16cc47f887954531
hirotaka-hakuta <hakuta.hirotaka@gmail.com>
2021-03-22 04:32:20 +0000

    商品詳細にflash追加、cart_productに色々実装、cart_productsテーブルにpriceカラム追加

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,7 +3,7 @@ class Member < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+has_many :cart_products
 
         validates :email, uniqueness: true
         validates :encrypted_password, uniqueness: true
