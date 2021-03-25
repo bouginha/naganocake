19d64a275dc81104317621cd4ae6265b456a7f2c
hirotaka-hakuta <hakuta.hirotaka@gmail.com>
2021-03-25 08:06:53 +0000

    購入時アドレス追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,54 +14,29 @@ class OrdersController < ApplicationController
   def confirm
   			# params[:order][:address_op]
     @order=Order.new(order_params)
+  	@member = current_member
+  	@ads = @member.addresses
+  	@cart_products=current_member.cart_products
+  	@ads = @member.addresses
 
-		@member = current_member
-
-		@ads = @member.addresses
-	    @cart_products=current_member.cart_products
-		
-	
-		# @items = Order.billing_amount
-
-		#ordered_itemにデータ挿入
-			# item = []
-			# @items = @member.cart_products
-			# 	@items.each do |i|
-			# 		item << @order.ordered_items.build(product_id: i.product_id, price: i.price, quantity: i.quantity, product_status: 1)
-			# 	end
-			# OrderedItem.import item
-
-
-		@ads = @member.addresses
-		#@orderparams = Order.find(params[:id])
-		# @items = @orderparams.ordered_items
-
-		#ordered_itemにデータ挿入
-
-			if params[:order][:address_op]	== "1"
-				@order.address = @member.address
-				@order.postal_code = @member.postal_code
-				@order.delivery_name = @member.first_name+@member.last_name
-			elsif params[:order][:address_op] == "2"
-				@ad = @ads.find(params[:Address][:id])
-				@order.address = @ad.address
-				@order.postal_code = @ad.postal_code
-				@order.delivery_name = @member.name
-			elsif params[:order][:address_op] == "3"
-				# #addressテーブルに保存させる
-				@ad = Address.new
-				@ad.member_id = @member.id
-				# @ad.address = params[:address_op][:address]
-				# @ad.name = params[:address][:name]
-
-				@ad.postal_code = params[:address_op][:postal_code]
-				# @ad.phone = params[:address][:telephone]
-				@ad.save
-
-				# @order.address = params[:address_op][:address]
-
-				# @order.postal_code = params[:address_op][:postal_code]
-			end
+		if params[:order][:address_op]	== "1"
+			@order.address = @member.address
+			@order.postal_code = @member.postal_code
+			@order.delivery_name = @member.first_name+@member.last_name
+		elsif params[:order][:address_op] == "2"
+			@ad = Address.find_by(address: @order.address)
+			@order.postal_code = @ad.postal_code
+			@order.delivery_name = @ad.name
+		elsif params[:order][:address_op] == "3"
+			#addressテーブルに保存させる
+			@ad = Address.new
+      @ad.member_id = @member.id
+			@ad.address = params[:order][:address_new]
+			@order.address = @ad.address
+			@ad.name = @order.delivery_name
+			@ad.postal_code = @order.postal_code
+			@ad.save
+		end
 	
   end
 
@@ -71,6 +46,8 @@ class OrdersController < ApplicationController
     @order = Order.new(order_params)
 
     if @order.save
+	    @cart_product = current_member.cart_products
+    @cart_product.destroy_all
 		redirect_to order_thanks_path
 	else
 		render :confirm
@@ -80,7 +57,8 @@ class OrdersController < ApplicationController
   def new
     @cart_products=current_member.cart_products
     @order=Order.new
-    @member = current_member
+
+	@member = current_member
 		if @member.cart_products.blank?
  		    flash[:warning] = "カートが空です"
 			redirect_to cart_products_path
@@ -121,4 +99,7 @@ class OrdersController < ApplicationController
     def order_params
 	 	params.require(:order).permit(:member_id, :payment_method, :address, :postal_code,:delivery_name,:billing_amount,:shipping_cost,:received_status)
     end
+    # def address_params
+    #     params.require(:address).permit(:member_id,:name,:address,:postal_code)
+    # end
 end
