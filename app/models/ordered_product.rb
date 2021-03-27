c87edf85aafa0fd373fab7d0e82c70e2455b0f91
hirotaka-hakuta <hakuta.hirotaka@gmail.com>
2021-03-27 04:57:56 +0000

    合計金額

--- app/models/ordered_product.rb
+++ app/models/ordered_product.rb
@@ -1,7 +1,7 @@
 class OrderedProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
-  enum produciton_status: {"着手不可": 0,"製作待ち": 1,"製作中": 2,"製作完了": 3}
+
 
     # 製作ステータス（0=着手不可 / 1=製作待ち / 2=製作中 / 3=製作完了）
       enum production_status: {
@@ -10,4 +10,6 @@ class OrderedProduct < ApplicationRecord
       "製作中":2,
       "製作完了":3,
     },_prefix: true
-end
+    
+
+      end
